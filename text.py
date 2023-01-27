1e834eb8758a2e9f18e28cda740beb9a634755c5
Rory O'Connell <104739139+rory-oconnell@users.noreply.github.com>
2023-01-27 19:31:38 +0000

    Removed sensitive information

--- text.py
+++ text.py
@@ -7,18 +7,18 @@ GOOD_MORNING_QUOTES = [
     "Good morning! Today is a new day, make the most of it!",   
 ]
 
-cellphone = "+353874629533"
-twilio_number = "+15735153671"
-account = "AC9e9e80de3a30c90481869b9b9efb23ca"
-token = "f52326d27b7bd05e2f3eebcd5803a29c"
+cellphone = "Cell_Number"
+twilio_number = "Twilio_Number"
+account = "Account_SID"
+token = "Account_Token"
 client = Client(account, token)
 
 quote = GOOD_MORNING_QUOTES[random.randint(0, len(GOOD_MORNING_QUOTES)-1)]
 client.messages.create(to=cellphone, from_=twilio_number, body=quote)
 
 message = client.messages.create(
-    to="+353874629533", 
-    from_="+15735153671",
+    to="+Cell_Number", 
+    from_="+Twilio_Number",
     body="Hello from Python!")
 
 print(message.sid)
